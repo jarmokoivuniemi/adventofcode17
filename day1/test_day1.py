4db3b626b56b34f43c260a6ebc3f88dc4373698b
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-02 10:24:33 +0200

    day 1 & 2 done

---
+++ day1/test_day1.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+from nose.tools import assert_equal
+
+def captcha1(num_string):
+    num_list = make_num_list(num_string)
+    return sum(digits_matching_next_digit(num_list))
+
+def digits_matching_next_digit(num_list):
+    return (num_list[i] for i in range(-1, len(num_list) - 1) if num_list[i] == num_list[i+1])
+
+def make_num_list(num_string):
+    return [int(num) for num in num_string]
+
+def captcha2(num_string):
+    num_list = make_num_list(num_string)
+    return sum(num_list[i] for i in range(len(num_list)) if num_list[i] == num_list[sliced_index(num_list, i)])
+
+def sliced_index(num_list, i):
+    return int((len(num_list)/2 + i) % (len(num_list)))
+
+    
+
+class TestDay1(TestCase):
+    def test_part1(self):
+        assert_equal(0, captcha1('0'))
+        assert_equal(2, captcha1('11'))
+        assert_equal(3, captcha1('111'))
+        assert_equal(3, captcha1('1122'))
+        assert_equal(4, captcha1('1111'))
+        assert_equal(9, captcha1('91212129'))
+        assert_equal(1, captcha1('0110'))
+        assert_equal(4, captcha1('12211'))
+        assert_equal(28, captcha1('649713959682898259577777'))
+
+    def test_part1_works(self):
+        with open('puzzle.txt') as f:
+            assert_equal(1228, captcha1(str(f.read()).strip()))
+
+    def test_part2(self):
+        #assert_equal(4, captcha2('1111'))
+        assert_equal(6, captcha2('1212'))
+        assert_equal(0, captcha2('1221'))
+        assert_equal(12, captcha2('123123'))
+        assert_equal(4, captcha2('12131415'))
+
+    def test_part2_works(self):
+        with open('puzzle.txt') as f:
+            assert_equal(1238, captcha2(str(f.read()).strip()))
+
---
+++ day2/test_day2.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+from nose.tools import assert_equal
+from itertools import combinations
+
+def difference_checksum(spreadsheet):
+    return sum(difference(make_num_list(numbers)) for numbers in spreadsheet)
+
+
+def difference(num_list):
+    return max(num_list) - min(num_list)
+
+
+def make_num_list(num_string):
+    return [int(n) for n in num_string.split()]
+
+
+class TestDay2Part1(TestCase):
+
+    def test_difference(self):
+        assert_equal(8, difference(make_num_list('5 1 9 5')))
+
+    def test_it_wors(self):
+        with open('puzzle.txt') as f:
+            puzzle = [n.strip() for n in f.readlines() if n.strip()]
+            assert_equal(53460, difference_checksum(puzzle))
+
+
+def division_checksum(spreadsheet):
+    return sum(divide_evenly_divisible(make_num_list(numbers)) for numbers in spreadsheet)
+
+
+def divide_evenly_divisible(num_list):
+    return next(divide(a, b) for a, b in combinations(num_list, 2) if are_divisible(a, b))
+
+def are_divisible(n, num):
+    return max(n, num) % min(n, num) == 0
+
+def divide(n, num):
+    return int(max(n, num) / min(n, num))
+
+
+class TestDay2Part2(TestCase):
+    
+    def test_even_division(self):
+        assert_equal(4, divide_evenly_divisible(make_num_list('5 9 2 8')))
+        assert_equal(3, divide_evenly_divisible(make_num_list('9 4 7 3')))
+        assert_equal(2, divide_evenly_divisible(make_num_list('3 8 6 5')))
+
+    def test_it_wors(self):
+        with open('puzzle.txt') as f:
+            puzzle = [n.strip() for n in f.readlines() if n.strip()]
+            assert_equal(282, division_checksum(puzzle))
+
+
