99cd94264fa2032429460a4b775825c805f4311a
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-02 23:40:27 +0200

    ...

--- day2/test_day2.py
+++ day2/test_day2.py
@@ -4,21 +4,21 @@ from itertools import combinations
 
 
 def difference_checksum(spreadsheet):
-    return sum(difference(make_num_list(numbers)) for numbers in spreadsheet)
+    return sum(difference(num_list(item)) for item in spreadsheet)
 
 
 def difference(num_list):
     return max(num_list) - min(num_list)
 
 
-def make_num_list(num_string):
+def num_list(num_string):
     return [int(n) for n in num_string.split()]
 
 
 class TestDay2Part1(TestCase):
 
     def test_difference(self):
-        assert_equal(8, difference(make_num_list('5 1 9 5')))
+        assert_equal(8, difference(num_list('5 1 9 5')))
 
     def test_it_wors(self):
         with open('puzzle.txt') as f:
@@ -27,7 +27,7 @@ class TestDay2Part1(TestCase):
 
 
 def division_checksum(spreadsheet):
-    return sum(divide_evenly_divisible(make_num_list(numbers)) for numbers in spreadsheet)
+    return sum(divide_evenly_divisible(num_list(item)) for item in spreadsheet)
 
 
 def divide_evenly_divisible(num_list):
@@ -45,9 +45,9 @@ def divide(a, b):
 class TestDay2Part2(TestCase):
 
     def test_even_division(self):
-        assert_equal(4, divide_evenly_divisible(make_num_list('5 9 2 8')))
-        assert_equal(3, divide_evenly_divisible(make_num_list('9 4 7 3')))
-        assert_equal(2, divide_evenly_divisible(make_num_list('3 8 6 5')))
+        assert_equal(4, divide_evenly_divisible(num_list('5 9 2 8')))
+        assert_equal(3, divide_evenly_divisible(num_list('9 4 7 3')))
+        assert_equal(2, divide_evenly_divisible(num_list('3 8 6 5')))
 
     def test_it_wors(self):
         with open('puzzle.txt') as f:
