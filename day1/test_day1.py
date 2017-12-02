72662ed6c9703d1e376fde93f768afa5799fa71c
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-02 10:26:35 +0200

    cleanup day1

--- day1/test_day1.py
+++ day1/test_day1.py
@@ -5,20 +5,23 @@ def captcha1(num_string):
     num_list = make_num_list(num_string)
     return sum(digits_matching_next_digit(num_list))
 
+
 def digits_matching_next_digit(num_list):
     return (num_list[i] for i in range(-1, len(num_list) - 1) if num_list[i] == num_list[i+1])
 
+
 def make_num_list(num_string):
     return [int(num) for num in num_string]
+
 
 def captcha2(num_string):
     num_list = make_num_list(num_string)
     return sum(num_list[i] for i in range(len(num_list)) if num_list[i] == num_list[sliced_index(num_list, i)])
 
+
 def sliced_index(num_list, i):
     return int((len(num_list)/2 + i) % (len(num_list)))
 
-    
 
 class TestDay1(TestCase):
     def test_part1(self):
@@ -37,7 +40,7 @@ class TestDay1(TestCase):
             assert_equal(1228, captcha1(str(f.read()).strip()))
 
     def test_part2(self):
-        #assert_equal(4, captcha2('1111'))
+        assert_equal(4, captcha2('1111'))
         assert_equal(6, captcha2('1212'))
         assert_equal(0, captcha2('1221'))
         assert_equal(12, captcha2('123123'))
