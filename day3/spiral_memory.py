c46e6501f5f9cbc3cbc47ac04d3cee742a536b8a
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-03 13:34:08 +0200

    not liking this solution at all

--- day3/spiral_memory.py
+++ day3/spiral_memory.py
@@ -1,55 +1,58 @@
 class SpiralMemory:
     def __init__(self, max_slot):
-        self.memory = self.spiral(max_slot)
+        self.max_slot = max_slot
+        self._free_memory_slots = max_slot
+        self.row_length = int(pow(self.max_slot, 0.5))
+        self.square_one = int((self.row_length-1)/2)
+        self.spiral_memory()
 
     def calcualte_distance(self, data):
         x_goal, y_goal = self.find_location(1)
         x, y = self.find_location(data)
         return abs(abs((x_goal - x)) + abs((y_goal - y)))
 
+    def spiral_memory(self):
+        self.memory = self._init_memory_slots()
+        for roundnum in range(self.square_one, -1, -1):
+            self._spiral_left(roundnum)
+            self._spiral_up(roundnum)
+            self._spiral_right(roundnum)
+            self._spiral_down(roundnum)
 
-    def spiral(self, max_value):
-        result = []
-        size = int(pow(max_value, 0.5))
-        for i in range(size):
-            inner = []
-            for j in range(size):
-                inner.append(None)
-            result.append(inner)
+    def _spiral_left(self, roundnum):
+        i = self.square_one + roundnum
+        for j in range(self.row_length-1, -1, -1):
+            if not self.memory[i][j]:
+                self.memory[i][j] = self._free_memory_slots
+                self._free_memory_slots -= 1
 
-        middle = int((size-1)/2)
-        num = max_value
+    def _spiral_up(self, roundnum):
+        j = self.square_one - roundnum
+        for i in range(self.row_length-1, -1, -1):
+            if not self.memory[i][j]:
+                self.memory[i][j] = self._free_memory_slots
+                self._free_memory_slots -= 1
 
-        for roundnum in range(middle, -1, -1):
-            i = middle + roundnum
-            for j in range(size-1, -1, -1):
-                if not result[i][j]:
-                    result[i][j] = num
-                    num -= 1
+    def _spiral_right(self, roundnum):
+        i = self.square_one - roundnum - self.row_length
+        for j in range(self.row_length):
+            if not self.memory[i][j]:
+                self.memory[i][j] = self._free_memory_slots
+                self._free_memory_slots -= 1 
 
-            j = middle - roundnum
-            for i in range(size-1, -1, -1):
-                if not result[i][j]:
-                    result[i][j] = num
-                    num -= 1
+    def _spiral_down(self, roundnum):
+        j = self.square_one + roundnum
+        for i in range(self.row_length-1):
+            if not self.memory[i][j]:
+                self.memory[i][j] = self._free_memory_slots
+                self._free_memory_slots -= 1
 
-            i = middle - roundnum-size
-            for j in range(size):
-                if not result[i][j]:
-                    result[i][j] = num
-                    num -= 1 
-
-            j = middle + roundnum + 0
-            for i in range(size-1):
-                if not result[i][j]:
-                    result[i][j] = num
-                    num -= 1
-
-        return result
+    def _init_memory_slots(self):
+        return [[None for _ in range(self.row_length)] for _ in range(self.row_length)]
 
     def find_location(self, data):
-        for i in range(len(self.memory)):
-            for j in range(len(self.memory)):
+        for i in range(self.row_length):
+            for j in range(self.row_length):
                 if self.memory[i][j] == data:
                     return (i, j)
 
