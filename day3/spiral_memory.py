df2355e0cfc62a253e9e06aa55d139421154f8d0
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-05 15:57:32 +0200

    tweaks

--- day3/spiral_memory.py
+++ day3/spiral_memory.py
@@ -2,6 +2,7 @@
 ACCESS_POINT = 1
 UP, RIGHT, DOWN, LEFT = 0, 90, 180, 270
 DIRECTIONS = {UP: (-1, 0), RIGHT: (0, 1), DOWN: (1, 0), LEFT: (0, -1)}
+SIDES_BEFORE_TURN = 2
 
 class SpiralMemory:
     def __init__(self, max_slot, hack=False, puzzle=0):
@@ -13,25 +14,25 @@ class SpiralMemory:
         self.row_length = int(pow(self.max_slot, 0.5))
         self.square_one = int((self.row_length-1)/2)
         self.memory = self._init_memory_slots()
-        self.fill_memory_slots()
+        self.make_spiral_memory()
 
     def calcualte_distance(self, data):
         x_goal, y_goal = self.find_location(ACCESS_POINT)
         x, y = self.find_location(data)
         return abs(abs((x_goal - x)) + abs((y_goal - y)))
 
-    def fill_memory_slots(self):
+    def make_spiral_memory(self):
         x, y = self.square_one, self.square_one
         self.memory[x][y] = 1
         slots_to_fill = 1
         slot_value = 2
         while not self.found:
-            for _ in range(2):
+            for _ in range(SIDES_BEFORE_TURN):
                 for _ in range(slots_to_fill):
                     x, y = self._next_slot(x, y)
                     self.memory[x][y] = self._slot_value(x, y, slot_value)
                     slot_value +=1
-                self.direction = self._turn()
+                self._turn()
             slots_to_fill += 1
 
     def _next_slot(self, x, y):
@@ -40,14 +41,14 @@ class SpiralMemory:
         return x, y
 
     def _turn(self):
-        return (self.direction + 270) % 360
+        self.direction = (self.direction + 270) % 360
 
     def _init_memory_slots(self):
         return [[0 for _ in self.row_range] for _ in self.row_range]
 
     def find_location(self, data):
-        return next((i, j) for i in self.row_range for j in self.row_range 
-                if self.memory[i][j] == data)
+        return next((x, y) for x in self.row_range for y in self.row_range 
+                if self.memory[x][y] == data)
 
     @property
     def row_range(self):
