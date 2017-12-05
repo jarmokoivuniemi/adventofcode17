986451e87a527bdb3b8035aad2f7332d47346dc4
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-05 08:21:28 +0200

    day 5 part 2 done

--- day5/test_day5.py
+++ day5/test_day5.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from nose.tools import assert_equal
 
-def steps_to_exit(steps):
+def steps_to_exit(steps, next_position):
     exit = len(steps)
     current_pos = 0
     step_map = {i: number for i, number in enumerate(steps)}
@@ -11,19 +11,34 @@ def steps_to_exit(steps):
         num_steps +=1
     return num_steps
 
-def next_position(current_pos, steps):
+
+def part1(current_pos, steps):
     pos = steps[current_pos]
     steps[current_pos] += 1
+    return pos
+
+
+def part2(current_pos, steps):
+    pos = steps[current_pos]
+    steps[current_pos] += -1 if pos >=3 else 1
     return pos
 
 class TestDay5(TestCase):
 
     def test_part1_example(self):
         steps = [0, 3, 0, 1, -3]
-        assert_equal(5, steps_to_exit(steps))
+        assert_equal(5, steps_to_exit(steps, part1))
     
     def test_part1_works(self):
         with open('puzzle.txt') as f:
             steps = [int(n.strip()) for n in f.readlines()]
-            assert_equal(373543, steps_to_exit(steps))
+            assert_equal(373543, steps_to_exit(steps, part1))
+
+    def test_part2_example(self):
+        steps = [0, 3, 0, 1, -3]
+        assert_equal(10, steps_to_exit(steps, part2))
         
+    def test_part2_works(self):
+        with open('puzzle.txt') as f:
+            steps = [int(n.strip()) for n in f.readlines()]
+            assert_equal(27502966, steps_to_exit(steps, part2))
