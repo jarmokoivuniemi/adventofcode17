c7721cd50341f6f44114453f80d9145474b9c5e6
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-06 09:04:55 +0200

    ha ha

--- day3/spiral_memory.py
+++ day3/spiral_memory.py
@@ -6,9 +6,9 @@ SIDES_BEFORE_TURN = 2
 
 class SpiralMemory:
     def __init__(self, max_slot, hack=False, puzzle=0):
-        self.puzzle = puzzle #i give up
+        self.puzzle = puzzle
         self.found = False
-        self.part2_hack = hack
+        self.part2_hack = hack #sigh
         self.direction = RIGHT
         self.max_slot = max_slot
         self.row_length = int(pow(self.max_slot, 0.5))
@@ -44,14 +44,14 @@ class SpiralMemory:
         self.direction = (self.direction + 270) % 360
 
     def _init_memory_slots(self):
-        return [[0 for _ in self.row_range] for _ in self.row_range]
+        return [[0 for _ in self._row_range] for _ in self._row_range]
 
     def find_location(self, data):
-        return next((x, y) for x in self.row_range for y in self.row_range 
+        return next((x, y) for x in self._row_range for y in self._row_range
                 if self.memory[x][y] == data)
 
     @property
-    def row_range(self):
+    def _row_range(self):
         return range(self.row_length)
 
     def _slot_value(self, x, y, slot_value):
@@ -62,12 +62,11 @@ class SpiralMemory:
         return result
 
     def _surrounding_elems(self, x, y):
-        return [self.memory[a][b] for a in self._range(x) for b in self._range(y) if (a, b) != (x, y)]
-
-    def _range(self, x):
-        if x == self.row_length-1:
-            return range(x-1, x+1)
-        return range(x-1, x+2) if x > 0 else range(x, x+2)
-
-
-
+        yield self.memory[x-1][y-1]
+        yield self.memory[x-1][y]
+        yield self.memory[x-1][y+1]
+        yield self.memory[x][y-1]
+        yield self.memory[x][y+1]
+        yield self.memory[x+1][y-1]
+        yield self.memory[x+1][y]
+        yield self.memory[x+1][y+1]
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -2,13 +2,14 @@ from unittest import TestCase
 from nose.tools import assert_equal
 from spiral_memory import SpiralMemory
 
+
 class TestDay3(TestCase):
 
     def test_part1_works(self):
-        memory = SpiralMemory(pow(600, 2), puzzle=312051)
+        memory = SpiralMemory(pow(1000, 2), puzzle=312051)
         assert_equal(430, memory.calcualte_distance(312051))
 
     def test_part2_works(self):
-        memory = SpiralMemory(pow(199, 2), hack=True, puzzle=312051)
+        memory = SpiralMemory(pow(1000, 2), hack=True, puzzle=312051)
         assert_equal(312453, memory.puzzle)
         
