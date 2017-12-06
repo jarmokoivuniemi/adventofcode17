7e66a3ec95dc30614bd5a3e08eb88feca3959f50
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-06 13:10:17 +0200

    pfft

--- day6/test_day6.py
+++ day6/test_day6.py
@@ -1,9 +1,9 @@
 from unittest import TestCase, skip
 from nose.tools import assert_equal
 
-def balanced_memory_banks(memory_banks, limit=2):
+def balanced_memory_banks(memory_banks):
     found_configs = []
-    while found_configs.count(memory_banks) < limit:
+    while found_configs.count(memory_banks) < 2:
         found_configs.append(list(balance(memory_banks)))
     return found_configs
 
@@ -13,6 +13,11 @@ def balance(memory_banks):
         memory_banks[(largest_bank + i) % len(memory_banks)] += 1
         memory_banks[largest_bank] -= 1
     return memory_banks
+
+
+def cycles_between_duplicates(found):
+    return len(found)-1 - found.index(found[len(found)-1])
+
 
 class TestDay6(TestCase):
 
@@ -29,10 +34,13 @@ class TestDay6(TestCase):
             assert_equal(14029, len(balanced_memory_banks(memory_banks)))
 
     def test_part2_examples(self):
-        assert_equal(5+4, len(balanced_memory_banks([0, 2, 7, 0], limit=3)))
+        memory_banks = [0, 2, 7, 0]
+        found = balanced_memory_banks(memory_banks)
+        assert_equal(4, cycles_between_duplicates(found))
 
     @skip('slow')
     def test_part2_works(self):
         with open('puzzle.txt') as f:
             memory_banks = [int(s.strip()) for s in f.read().split()]
-            assert_equal(14029 + 2765, len(balanced_memory_banks(memory_banks, limit=3)))
+            found = balanced_memory_banks(memory_banks)
+            assert_equal(2765, cycles_between_duplicates(found))
