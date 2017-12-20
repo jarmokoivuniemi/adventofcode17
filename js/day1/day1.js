7bed010cadafc88eebbf28926fbe5f9dedf8d9e7
JarmoKoivuniemi <koivuniemijarmo@gmail.com>
2017-12-20 07:26:45 +0200

    refactor day1

--- js/day1/day1.js
+++ js/day1/day1.js
@@ -1,18 +1,25 @@
 
 function captcha1(input) {
   const arr = intArray(input);
-  return  arr.reduce((res, x, i) => res + (x === arr[(i+1) % arr.length] ? x : 0), 0);
+  const isNextDigitMatch = (x, i) => x === arr[(i+1) % arr.length];
+  const digitsMatchingNextDigit = (sum, x, i) => sum + (isNextDigitMatch(x, i) ? x : 0)
+
+  return  arr.reduce(digitsMatchingNextDigit, 0);
+};
+
+function captcha2(input) {
+  const arr = intArray(input);
+  const half = arr.length/2;
+  const matchesHalfwayAround = (x, i) => x === arr[(i+half) % arr.length];
+  const digitsMatchingHalfwayAround = (sum, x, i) => sum + (matchesHalfwayAround(x, i) ? x : 0)
+
+  return  arr.reduce(digitsMatchingHalfwayAround, 0);
 };
 
 function intArray(input) {
   return input.split('').map(x => parseInt(x));
 };
 
-function captcha2(input) {
-  const arr = intArray(input);
-  const half = arr.length/2
-  return  arr.reduce((res, x, i) => res + (x === arr[(i+half) % arr.length] ? x : 0), 0);
-};
 
 module.exports.captcha1 = captcha1
 module.exports.captcha2 = captcha2
